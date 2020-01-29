7d98816b01a8722ad3ae5e07569b33fa6a8adcdb
nazarpunk <nazarpunk@ya.ru>
2020-01-29 15:48:57 +0200

    sdk

--- sdk/update.bat
+++ sdk/update.bat
@@ -1,1 +1,3 @@
-powershell -command -(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/nazarpunk/cheapack/master/lua/build.lua', 'lua/build.lua')
+powershell -command -(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/nazarpunk/cheapack/master/sdk/common.j.lua', 'common.j.lua')
+powershell -command -(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/nazarpunk/cheapack/master/sdk/blizzard.j.lua', 'blizzard.j.lua')
+powershell -command -(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/nazarpunk/cheapack/master/sdk/common.ai.j.lua', 'common.ai.j.lua')
